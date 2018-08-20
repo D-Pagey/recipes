c86082911bd1bc41f4f1f09394532180a4dac5a8
D-Pagey <dan.page91@gmail.com>
2018-08-21 00:01:41 +0100

    Config Firebase, can read and write to database.

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Experimenting with an idea to display favourite recipes.
 
 **To Do**
-- [ ] Move the data from local to Firebase
-- [ ] Read all recipes in Firebase
-- [ ] Create new recipes in Firebase
-- [ ] Delete recipes in Firebase
-- [ ] Update recipes in Firebase
+- Move the data from local to Firebase
+- Read all recipes in Firebase
+- Create new recipes in Firebase
+- Delete recipes in Firebase
+- Update recipes in Firebase
+- Hide config for firebase
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import firestore from '../firebase';
 import RecipeList from './RecipeList';
 
 const AppWrapper = styled.div`
@@ -13,11 +14,37 @@ const MainTitle = styled.h1`
   text-align: center;
 `;
 
-export default function App() {
-  return (
-    <AppWrapper>
-      <MainTitle>Recipes</MainTitle>
-      <RecipeList />
-    </AppWrapper>
-  );
+export default class App extends Component {
+  state = {
+    recipes: [],
+  }
+
+  componentDidMount() {
+    // firestore.collection('recipes').add({
+    //   name: 'Chicken Satay',
+    //   serves: 2,
+    //   prepTime: 12,
+    // })
+    //   .then((docRef) => {
+    //     console.log('Document written with ID: ', docRef.id);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error adding document: ', error);
+    //   });
+    firestore.collection('recipes').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data()}`);
+        console.log(doc.data());
+      });
+    });
+  }
+
+  render() {
+    return (
+      <AppWrapper>
+        <MainTitle>Recipes</MainTitle>
+        <RecipeList />
+      </AppWrapper>
+    );
+  }
 }
