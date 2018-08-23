21548325044325772675f2806c7b0b9d7246bf81
D-Pagey <dan.page91@gmail.com>
2018-08-23 23:58:57 +0100

    WIP Starting to add delete functionality.

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -14,25 +14,33 @@ const RecipeTitle = styled.h4`
   text-align: center;
 `;
 
-export default function RecipeCard({ recipe }) {
-  const {
-    name, prepTime, serves,
-  } = recipe;
+export default class RecipeCard extends Component {
+  deleteRecipe = () => {
+    console.log('deleting recipe');
+  }
 
-  return (
-    <RecipeCardWrapper>
-      <RecipeTitle>{name}</RecipeTitle>
-      <p>Preparation Time = <span>{prepTime} minutes</span></p>
-      <p>Serves = <span>{serves}</span></p>
-    </RecipeCardWrapper>
-  );
+  render() {
+    const {
+      name, prepTime, serves,
+    } = this.props.recipe;
+
+    return (
+      <RecipeCardWrapper>
+        <RecipeTitle>{name}</RecipeTitle>
+        <p>Preparation Time = <span>{prepTime} minutes</span></p>
+        <p>Serves = <span>{serves}</span></p>
+        <button type="button" onClick={this.deleteRecipe}>
+          <i className="material-icons">delete_forever</i>
+        </button>
+      </RecipeCardWrapper>
+    );
+  }
 }
 
 RecipeCard.propTypes = {
   recipe: PropTypes.shape({
-    title: PropTypes.string,
-    cookingTime: PropTypes.number,
-    preparationTime: PropTypes.number,
+    name: PropTypes.string,
+    prepTime: PropTypes.number,
     serves: PropTypes.number,
   }).isRequired,
 };
